89f025d1ba79f2c0e9ed7f2e8435c78993435131
Youssef Adel <youssefelking2022@gmail.com>
2022-03-18 00:30:23 +0200

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { borderLeftColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import Task from './components/Task';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today,s Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          {/*this is where the tasks will go! */}
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#5f9ea0',
   },
+  tasksWrapper:{
+    paddingTop: 50,
+    paddingHorizontal:20,
+
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight:'bold',
+
+  },
+  items:{
+    marginTop:30
+  }
 });
